47b551c85068eae121af6e6071552be6f9d5db09
FatinShamas <fatin.shamas786@gmail.com>
2023-11-20 08:27:19 +0800

    commit 2

--- js/main.js
+++ js/main.js
@@ -90,5 +90,26 @@
     });
 
     
+
+    
+
+    
 })(jQuery);
 
+
+
+
+function prev(){
+    document.getElementById('slider-container').scrollLeft -= 270;
+}
+
+function next()
+{
+    document.getElementById('slider-container').scrollLeft += 270;
+}
+
+
+$(".slide img").on("click" , function(){
+$(this).toggleClass('zoomed');
+$(".overlay").toggleClass('active');
+})
